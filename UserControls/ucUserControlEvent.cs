8d3a8e79a5079000ce3b21972a1b951a71f3d0c4
porcorosso21 <porcorosso21@users.noreply.github.com>
2025-03-10 17:12:15 +0800

    Create ucUserControlEvent.cs

---
+++ UserControls/ucUserControlEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dotNetExample.UserControls
+{
+    public partial class ucUserControlEvent: UserControl
+    {
+        public EventHandler LabelClick;
+
+        public ucUserControlEvent()
+        {
+            InitializeComponent();
+
+            //綁定label的Click事件，並指定觸發LabelClick
+            label1.Click += new EventHandler((s, e) =>
+            {
+                if (this.LabelClick != null)
+                {
+                    LabelClick(s, e);
+                }
+            });
+        }
+
+
+    }
+}
