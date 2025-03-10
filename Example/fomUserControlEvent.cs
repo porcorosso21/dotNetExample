fad9e9ca9dde513933536791c54fd48e1c69e615
porcorosso21 <porcorosso21@users.noreply.github.com>
2025-03-10 17:12:07 +0800

    Create fomUserControlEvent.cs

---
+++ Example/fomUserControlEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dotNetExample.Example
+{
+    public partial class fomUserControlEvent: Form
+    {
+        public fomUserControlEvent()
+        {
+            InitializeComponent();
+
+            ucUserControlEvent1.LabelClick += new EventHandler((s, e) =>
+            {
+                MessageBox.Show(String.Format("點擊到UserControl中的Label了!!Label的文字為「{0}」", ((Label)s).Text));
+            });
+        }
+    }
+}
